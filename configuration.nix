bdda6e0e69fd8a64064ecdc2a46762f280d10845
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2026-06-09 21:34:51 +0300

    h

--- configuration.nix
+++ configuration.nix
@@ -44,6 +44,7 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
+    powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
