1e8d0be6fd8857e08f218a44d966a344ffbdf615
Ahmet Yusuf Demir <ahmetyusufdmr88@gmail.com>
2026-06-10 17:15:32 +0300

    h

--- configuration.nix
+++ configuration.nix
@@ -45,6 +45,7 @@
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.finegrained = false;
+    dynamicBoost.enable = true;
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
--- home.nix
+++ home.nix
@@ -68,8 +68,6 @@
     obs-studio
     jetbrains.idea-oss
     vesktop
-    mangohud
-    goverlay
     zulu21
 
     # GNOME Extensions
@@ -253,6 +251,41 @@
         "alt+digit_7=goto_tab:7"
         "alt+digit_8=goto_tab:8"
       ];
+    };
+  };
+
+  programs.mangohud = {
+    enable = true;
+    settings = {
+      fps = true;
+      frametime = true;
+      frame_timing = true;
+
+      gpu_stats = true;
+      gpu_temp = true;
+      gpu_core_clock = true;
+      gpu_power = true;
+      throttling_status = true;
+
+      cpu_stats = true;
+      cpu_temp = true;
+      cpu_mhz = true;
+      cpu_power = true;
+
+      ram = true;
+      vram = true;
+
+      fps_metrics = "avg,0.01";
+      fps_limit = "0,60,144";
+      toggle_fps_limit = "Shift_L+F1";
+
+      position = "top-left";
+      hud_compact = true;
+      font_size = 20;
+      background_alpha = 0.4;
+
+      no_display = false;
+      toggle_hud = "Shift_R+F12";
     };
   };
 }
